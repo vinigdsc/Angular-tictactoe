a60a848a3ef112375c0ad5ad92d97e112722add0
Vinicius Caetano <viniciusgcae@gmail.com>
2022-11-04 14:13:53 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicTacToeComponent
   ],
   imports: [
     BrowserModule
